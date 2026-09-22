85d920f5487b2a7ff0c9b1cfbffdefd46666edb1
bfue99 <brandonfuentes@me.com>
2026-09-22 09:51:56 -0700

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -6,7 +6,7 @@ Student::void printStanding(string name, int year) {
     if (year == 1)
         cout << name << " is a Freshman" << endl;
     else if (year == 2)
-        cout << name << " is a Sophmore" << endl;
+        cout << name << " is a Sophomore" << endl;
     else if (year == 3)
         cout << name << " is a Junior" << endl;
     else if (year == 4)
